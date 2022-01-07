393063a66b5ea67e097abf461cccefd63544c14f
HO_THI_THOM <thom1622000@gmail.com>
2022-01-07 17:19:56 +0700

    commit

---
+++ Webform/Models/KetQua.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Webform.Models
+{
+    public class KetQua
+    {
+        public String KhoaThi { set; get; }
+        public String Sbd { set; get; }
+        public NguoiDung NguoiDung{ set; get; }
+        public PhongThi PhongThi{ set; get; }
+    }
+}
